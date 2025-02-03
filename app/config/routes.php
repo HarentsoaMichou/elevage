6d891914bb02e951400df154bfdffe7cfbe713c5
HarentsoaMichou <harentsoamichou@gmail.com>
2025-02-03 15:31:17 +0300

    Initial commit

---
+++ app/config/routes.php
@@ -0,0 +1,15 @@
+<?php
+use app\controllers\UserController;
+use app\controllers\DepotController;
+use app\controllers\CadeauController;
+
+use flight\Engine;
+use flight\net\Router;
+
+
+/** 
+ * @var Router $router 
+ * @var Engine $app
+ */
+
+
